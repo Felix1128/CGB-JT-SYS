42537fb5289eca8a0cac00ed01fa96f91ac3c93b
xue.li <xue.li@heweisoft.com>
2019-03-21 22:16:53 +0800

    权限管理系统

---
+++ CGB-JT-SYS-V1.01/src/main/java/com/jt/test/TestGitConnection.java
@@ -0,0 +1,7 @@
+package com.jt.test;
+
+public class TestGitConnection {
+	/**
+	 * 测试git push 是否正确！
+	 */
+}
---
+++ CGB-JT-SYS-V1.01/src/test/java/com/test/TestGit1.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class TestGit1 {
+
+	/**
+	 * 测试 Git 
+	 */
+}
